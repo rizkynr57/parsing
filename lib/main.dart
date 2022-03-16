0faace8c98a4d9768edd107292df48b89a68472c
Bimaputr <bima_235@smkassalaambandung.sch.id>
2022-03-16 09:06:13 +0700

    flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:rizkyn_perawatan_komputer/sample_json.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,18 +9,16 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
-  Widget build(BuilsXontext context) {
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: MyHomePage(),
     );
-  } 
+  }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -28,42 +29,41 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: text('Read Json'),
+        title: const Text('Read Json'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisAlignment.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDoctor(
-                    onTap: () {},
-                    child: Container(
-                      height: 50,
-                      width: 150,
-                      color: Colors.grey,
-                      alignment: Alignment.center,
-                      child: const Text('Read JSON File'),
-                    ),
+          child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {},
+                  child: Container(
+                    height: 50,
+                    width: 150,
+                    color: Colors.grey,
+                    alignment: Alignment.center,
+                    child: const Text("Read Json File"),
                   ),
-                ],
-              ),
-              Padding(
-                padding: const EdgeInset.only(top: 12.0),
-                child: Text(
-                  _jsonContent,
-                  TextAlign: TextAlign.center,
                 ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12.0),
+              child: Text(
+                _jsonContent,
+                textAlign: TextAlign.center,
               ),
-            ],
-          ), 
+            )
+          ],
         ),
-      ),
+      )),
     );
   }
 }
