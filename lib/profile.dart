56dd5e83f068e5033066a18a56f00b5da5e8e06f
rizkynr57 <rizky.nurahman48@gmail.com>
2022-03-17 10:50:49 +0700

    flutter

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,21 +5,24 @@ import 'package:flutter/services.dart';
 
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
+
   @override
   State<Profile> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<Profile> {
+  late String _jsonContent = "";
   late String _name = "";
   late String _age = "";
   late String _hobi = "";
   late String _username = "";
   late String _repo = "";
-  Future _loadJson() async {
+  Future _loadSampleJson() async {
     String jsonString = await rootBundle.loadString("assets/sample.json");
     final jsonData = json.decode(jsonString);
     Sample sample = Sample.fromJson(jsonData);
     setState(() {
+      _jsonContent = sample.toString();
       _name = sample.name.toString();
       _age = sample.age.toString();
       _hobi = sample.hobi.toString();
@@ -33,6 +36,7 @@ class _ProfilePageState extends State<Profile> {
     return MaterialApp(
       home: Scaffold(
         body: Container(
+          margin: EdgeInsets.only(top: 40.0),
           child: ListView(
             children: [
               Container(
@@ -42,47 +46,74 @@ class _ProfilePageState extends State<Profile> {
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
-              Container(
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100.0),
-                    image: DecorationImage(
-                        image: AssetImage('assets/img/profile.png'),
-                        fit: BoxFit.cover),
-                  ),
-                ),
-              ),
               Padding(padding: EdgeInsets.only(top: 15)),
               Container(
-                width: 500,
-                height: 500,
+                width: 400,
+                height: 300,
+                decoration: BoxDecoration(
+                  image:
+                      DecorationImage(image: AssetImage("assets/img/tema.png")),
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(top: 25)),
+              Container(
+                width: 400,
+                height: 250,
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: <Color>[
+                        Colors.deepPurple,
+                        Colors.deepPurpleAccent
+                      ],
+                    ),
+                    borderRadius: BorderRadius.circular(25)),
                 child: Column(
                   children: [
+                    Padding(padding: EdgeInsets.only(top: 50)),
                     Container(
-                      width: 150,
+                      padding: EdgeInsets.all(10),
+                      width: 380,
                       height: 60,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           border: Border.all(color: Colors.white)),
-                      child: Text("Nama"),
+                      child: Text(
+                        "Nama : " + _name,
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
                     ),
                     Container(
-                      width: 150,
+                      padding: EdgeInsets.all(10),
+                      width: 380,
                       height: 60,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           border: Border.all(color: Colors.white)),
-                      child: Text("Umur"),
+                      child: Text(
+                        "Umur : " + _age,
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
                     ),
                     Container(
-                      width: 150,
+                      padding: EdgeInsets.all(10),
+                      width: 380,
                       height: 60,
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20),
                           border: Border.all(color: Colors.white)),
-                      child: Text("Hobi"),
+                      child: Text(
+                        "Hobi : " + _hobi,
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
                     ),
                   ],
                 ),
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:rizkyn_perawatan_komputer/main.dart';
+import 'package:data_parsing/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
